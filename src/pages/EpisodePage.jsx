1cd5ac5b461931907ac020f55d0531d7a4ef9a2b
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-09-23 19:01:34 +0200

    implemented season filtering for episodes list

--- src/pages/EpisodePage.jsx
+++ src/pages/EpisodePage.jsx
@@ -1,9 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 
 export default function EpisodePage() {
 
     const { episodes, info } = useContext(GlobalContext);
+    const [season, setSeason] = useState("1");
+
 
     if (!episodes || episodes.length === 0) {
         return <p>No episodes found...</p>
@@ -12,12 +14,37 @@ export default function EpisodePage() {
     return (
         <>
             <h1>Lista episodi</h1>
-            <ul>
-                {episodes.map((episode) => (
-                    <li key={episode.id}>
-                        <h3>{episode.episode} - {episode.name}</h3>
-                    </li>
-                ))}
+            <select
+                id="season"
+                onChange={(e) => setSeason(e.target.value)}
+                value={season}
+            >
+                <option value="1">Stagione 1</option>
+                <option value="2">Stagione 2</option>
+            </select>
+            <ul className="episode-list">
+                {season === "1"
+                    ? (
+                        episodes
+                            .filter(episode => episode.episode.startsWith("S01"))
+                            .map((e) => (
+                                < li key={e.id} >
+                                    <h3>{e.episode}</h3>
+                                    <p>{e.name}</p>
+                                </li>
+                            ))
+                    )
+                    : (
+                        episodes
+                            .filter(e => e.episode.startsWith("S02"))
+                            .map((e) => (
+                                < li key={e.id} >
+                                    <h3>{e.episode}</h3>
+                                    <p>{e.name}</p>
+                                </li>
+                            ))
+                    )
+                }
             </ul>
         </>
     )
