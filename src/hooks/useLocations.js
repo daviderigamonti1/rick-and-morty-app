30cc28e344a608c07a78f2a6fe97796d736bec40
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-09-29 18:46:07 +0200

    implemented hook to fetch all locations

--- src/hooks/useLocations.js
+++ src/hooks/useLocations.js
@@ -7,9 +7,17 @@ export default function useLocations() {
     useEffect(() => {
         async function fetchLocations() {
             try {
-                const res = await fetch("https://rickandmortyapi.com/api/location");
-                const data = await res.json();
-                setLocations(data.results);
+                const firstRes = await fetch("https://rickandmortyapi.com/api/location");
+                const firstData = await firstRes.json();
+                const totalPages = firstData.info.pages;
+
+                let promises = [];
+                for (let i = 1; i <= totalPages; i++) {
+                    promises.push(fetch(`https://rickandmortyapi.com/api/location?page=${i}`).then(res => res.json()));
+                }
+                const allData = await Promise.all(promises);
+                const mergedData = allData.flatMap((data) => data.results);
+                setLocations(mergedData);
             } catch (err) {
                 console.error(err);
             }
