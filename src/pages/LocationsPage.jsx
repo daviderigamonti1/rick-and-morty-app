8fb9a61cfdd9abff0d4c566f2fcdc0e1f9fa293b
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-09-30 20:23:02 +0200

    improved locations page with search bar and pagination

--- src/pages/LocationsPage.jsx
+++ src/pages/LocationsPage.jsx
@@ -1,27 +1,60 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../context/GlobalContext";
 
 export default function LocationsPage() {
     const { locations } = useContext(GlobalContext);
 
+    const [searchTitle, setSearchTitle] = useState("");
+    const [page, setPage] = useState(1);
+
     const navigate = useNavigate();
+
+    const filteredLocations = locations.filter((location) => {
+        return location.name.toLowerCase().includes(searchTitle.toLowerCase())
+    })
 
     if (!locations || locations.length === 0) {
         return <p>Nessun luogo trovato</p>
     }
 
+    const itemsPerPage = 20;
+    const startIndex = (page - 1) * itemsPerPage;
+    const currentLocations = filteredLocations.slice(
+        startIndex,
+        startIndex + itemsPerPage
+    );
+
     return (
         <>
             <h2>Lista dei luoghi</h2>
+            <input
+                type="text"
+                placeholder="Cerca un luogo..."
+                onChange={e => {
+                    setSearchTitle(e.target.value)
+                    setPage(1)
+                }}
+            />
             <ul className="locations-list">
-                {locations.map((l) => (
+                {currentLocations.map((l) => (
                     <li key={l.id}>
                         <p>{l.name}</p>
                         <button onClick={() => navigate(`/locations/${l.id}`)}>Vedi dettagli</button>
                     </li>
                 ))}
             </ul>
+            <div className="pagination">
+                <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+                    ◀️ Pagina precedente
+                </button>
+                <span>
+                    Pagina {page} di {Math.ceil(filteredLocations.length / itemsPerPage)}
+                </span>
+                <button onClick={() => setPage(page + 1)} disabled={page >= Math.ceil(filteredLocations.length / itemsPerPage)}>
+                    Pagina successiva ▶️
+                </button>
+            </div>
         </>
     )
 }
